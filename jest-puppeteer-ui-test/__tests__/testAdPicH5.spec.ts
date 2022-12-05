70283937d3de69c449fe7a0bbd2f74b409a0f497
joycesong <joycesong@tencent.com>
2022-12-05 21:58:42 +0800

    update case

--- jest-puppeteer-ui-test/__tests__/testAdPicH5.spec.ts
+++ jest-puppeteer-ui-test/__tests__/testAdPicH5.spec.ts
@@ -4,7 +4,6 @@ import {PageExtend} from "../lib/search-page/page-extend";
 import { adActivityClass, wxAdClass } from "../lib/utils/resultMap";
 import { addAttach, addMsg } from "jest-html-reporters/helper";
 import { errorCounting, finderOperation, getHeightOfEle, superView } from "../lib/utils/helper";
-import fs from "fs";
 
 let page: Puppeteer.Page ;
 let browser:  Puppeteer.Browser;
